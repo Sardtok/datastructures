42a54ec46d437b8afca32b076500657d9f2a49d4
Sigmund Hansen <sigmunha@ifi.uio.no>
2013-12-21 11:50:41 +0100

    Adds license text to linked list test.

--- java/test/com/sigmund_hansen/datastructures/LinkedListTest.java
+++ java/test/com/sigmund_hansen/datastructures/LinkedListTest.java
@@ -1,7 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * The data structures language practice project.
+ * Copyright (C) 2013 Sigmund Hansen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.sigmund_hansen.datastructures;
 
 import java.util.ArrayList;
@@ -18,7 +32,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * Tests for the implementation.
+ * 
  * @author Sigmund Hansen
  */
 public class LinkedListTest {
