f73cb9e1e032f4215949c4b1f0eb533a1031d401
Sigmund Hansen <sigmunha@ifi.uio.no>
2013-12-21 11:58:16 +0100

    Updates license text to fit multi-file project.

--- java/src/com/sigmund_hansen/datastructures/LinkedList.java
+++ java/src/com/sigmund_hansen/datastructures/LinkedList.java
@@ -2,18 +2,20 @@
  * The data structures language practice project.
  * Copyright (C) 2013 Sigmund Hansen
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of Data Structures Language Practice Project, DSLPP.
+ * 
+ * DSLPP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * DSLPP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with DSLPP.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.sigmund_hansen.datastructures;
